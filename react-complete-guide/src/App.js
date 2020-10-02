f6d617f33ac3a2ce39fa1d0e90fc12e47db1ae4f
niCognition <Niclas.p1990@gmail.com>
2020-10-02 10:51:41 +0200

    push latest changes from course

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,12 +1,25 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
+state = {
+  persons: [
+    { name: 'Niclas', age: 30 },
+    { name: 'Herman', age: 27 },
+    { name: 'Sebastian', age: 32}
+  ]
+}
+
   render() {
     return (
       <div className="App">
           <h1>Hi, I'm a React App</h1>
           <p>This is really working!</p>
+          <button>Switch Name</button>
+          <Person name={this.state.persons[0].name} age={this.state.persons[0].age}>Har Rövgänget under uppsikt aka chiefen</Person>
+          <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>Utvecklare för Rövänget</Person>
+          <Person name={this.state.persons[2].name} age={this.state.persons[2].age}>Teknisk specialist och utvecklare för Rövgänget</Person>
       </div>
     );
   }
