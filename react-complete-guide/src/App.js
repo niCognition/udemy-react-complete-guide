a86c32f63ee3c786a15e06da93ab22d3a3c9b67a
niCognition <Niclas.p1990@gmail.com>
2020-10-02 15:32:06 +0200

    add latest changes from udemy course

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,28 +1,78 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-class App extends Component {
-state = {
-  persons: [
-    { name: 'Niclas', age: 30 },
-    { name: 'Herman', age: 27 },
-    { name: 'Sebastian', age: 32}
-  ]
-}
+const app = props => {
+  const [personsState, setPersonsState] = useState({
+    persons: [
+      { name: 'Niclas', age: 30 },
+      { name: 'Herman', age: 27 },
+      { name: 'Sebastian', age: 32 }
+    ],
+  });
 
-  render() {
-    return (
-      <div className="App">
-          <h1>Hi, I'm a React App</h1>
-          <p>This is really working!</p>
-          <button>Switch Name</button>
-          <Person name={this.state.persons[0].name} age={this.state.persons[0].age}>Har Rövgänget under uppsikt aka chiefen</Person>
-          <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>Utvecklare för Rövänget</Person>
-          <Person name={this.state.persons[2].name} age={this.state.persons[2].age}>Teknisk specialist och utvecklare för Rövgänget</Person>
-      </div>
-    );
+  const [otherState, setOtherState] = useState('some other value');
+
+  console.log(personsState, otherState);
+
+  const switchNameHandler = (newName) => {
+    // console.log('Was clicked!');
+    // DON'T DO THIS: this.state.persons[0].name = 'Nico';
+    setPersonsState({
+      persons: [
+        { name: newName, age: 413 },
+        { name: 'Hentai-Kun', age: 80085 },
+        { name: 'Sebastian-senpai', age: 1337 }
+      ],
+      otherState: personsState.otherState
+    });
   }
+
+  const nameChangedHandler = (event) => {
+    setPersonsState({
+      persons: [
+        { name: 'Vim-Sama!', age: 413 },
+        { name: event.target.value, age: 80085 },
+        { name: 'Sebastian-senpai', age: 1337 }
+      ],
+      otherState: personsState.otherState
+    });
+  }
+
+  const style = {
+    backgroundColor: 'white',
+    font: 'inherit',
+    border: '1px solid blue',
+    padding: '8px',
+    cursor: 'pointer'
+  };
+
+  return (
+    <div className="App">
+      <h1>Hi, I'm a React App</h1>
+      <p>This is really working!</p>
+      <button 
+        style={style}
+        onClick={() => switchNameHandler('Vim-Dono!!')}>Switch Name</button>
+      <Person
+        name={personsState.persons[0].name}
+        age={personsState.persons[0].age}
+        click={switchNameHandler.bind('Nico!')}>
+        Har Rövgänget under uppsikt aka chiefen
+      </Person>
+      <Person
+        name={personsState.persons[1].name}
+        age={personsState.persons[1].age}
+        changed={nameChangedHandler}>
+        Utvecklare för Rövänget
+      </Person>
+      <Person
+        name={personsState.persons[2].name}
+        age={personsState.persons[2].age}>
+        Teknisk specialist och utvecklare för Rövgänget
+      </Person>
+    </div>
+  );
 }
 
-export default App;
+export default app;
